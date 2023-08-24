c778a4a44b15c272cbe7962742362d4e7dec5314
Ekbal41 <mdasifekbalshagor@gmail.com>
2023-08-25 00:27:47 +0600

    ok

--- examples/basic/ends.js
+++ examples/basic/ends.js
@@ -3,6 +3,13 @@ module.exports = napnux()
   .get("/", (req, res) => {
     res.render("hello");
   })
+  .get("/send", (req, res) => {
+    res.send("Hello World!");
+    // res.send({
+    //   message: "Hello World!",
+    // });
+
+  })
   .get("/hello/:name", (req, res) => {
     res.end(`Hello ${req.params.name}!`);
   });
