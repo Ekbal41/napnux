49fbad065858e4ff6376ea46f4bf1707f19c8387
Ekbal41 <mdasifekbalshagor@gmail.com>
2023-08-26 01:09:36 +0600

    ok

--- examples/basic/ends.js
+++ examples/basic/ends.js
@@ -1,15 +1,19 @@
 const napnux = require("../../index.js");
+
 module.exports = napnux()
   .get("/", (req, res) => {
     res.render("hello");
   })
+  .post("/", (req, res) => {
+    console.log("req.body", req.body);
+    res.redirect("/");
+  })
+
   .get("/send", (req, res) => {
     res.send("Hello World!");
-    // res.send({
-    //   message: "Hello World!",
-    // });
-
   })
   .get("/hello/:name", (req, res) => {
+    console.log(req.search);
+    console.log(req.query);
     res.end(`Hello ${req.params.name}!`);
   });
