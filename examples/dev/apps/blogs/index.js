7b7b2ab44f92b33a638a4c7c2dbc0ccee8211a0c
Ekbal41 <mdasifekbalshagor@gmail.com>
2023-12-01 18:47:51 +0600

    `symlink` bug fixed👨‍💻

--- examples/dev/apps/blogs/index.js
+++ examples/dev/apps/blogs/index.js
@@ -1,8 +1,6 @@
 const napnux = require("../../../../index.js");
 
 module.exports = napnux()
-  // .static(__dirname + "/extraPublic")
-  // .views(__dirname + "/extraViews")
   .get("/", (req, res) => {
     res.render("hello");
   });
--- examples/dev/index.js
+++ examples/dev/index.js
@@ -5,12 +5,8 @@ const server = require("./ends.js");
 let s = server
   .use("/notes", notes)
   .use("/blogs", blogs)
-  // .views(__dirname + "/projectViews")
-  // .static(__dirname + "/projectPublic")
-  // .static(__dirname + "/public")
-
-  
-  // .set("viewsDir", "/projectViews")
+  .views(__dirname + "/views")
+  .static(__dirname + "/public")
   .start(3000, () => {
     console.log("> Server Listening on http://localhost:3000");
   });
